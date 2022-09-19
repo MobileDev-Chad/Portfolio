105abef5184dd97f997cea40ec90953fb3602259
WebDev-Chad <chadwin.allotey@gmail.com>
2022-09-18 21:50:05 -0400

    Update SkillsList.js

--- src/data/SkillsList.js
+++ src/data/SkillsList.js
@@ -56,7 +56,7 @@ export const SkillsList = [
     id: 9,
   },
   {
-    icon:  <Img src={Svg1} alt="Firebase" height="50" width="50" class="filter-orange"/>,
+    icon:  <img src={Svg1} alt="Firebase" height="50" width="50" className="filter-orange"></img>,
     heading: "Firebase",
     id: 10,
   },
