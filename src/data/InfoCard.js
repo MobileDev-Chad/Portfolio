5030e857b4729f6ac2bb0ebc67c8f5ef118ee8ae
WebDev-Chad <chadwin.allotey@gmail.com>
2022-12-05 01:24:48 -0500

    Update InfoCard.js

--- src/data/InfoCard.js
+++ src/data/InfoCard.js
@@ -1,7 +1,7 @@
 export const InfoCard = [
   { title: "Name:", text: "Chadwin Allotey", id: 1 },
   { title: "Email:", text: "chadwin.allotey@gmail.com", id: 2 },
-  { title: "Date of birth:", text: "29 Octtober,1993", id: 3 },
+  { title: "Date of birth:", text: "29 October,1993", id: 3 },
   { title: "From:", text: "Mississauga, ON.", id: 4 },
 ];
 export default InfoCard;
